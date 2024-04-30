da0fdd7311cf69d7aa045362214d519458ab127b
nightpanda2810 <nightpanda2810@gmail.com>
2024-04-30 12:56:23 -0400

    Refactored function to be more readable. Added hints. Minor adjustments to conform to Pylint.

--- modules/extract_data.py
+++ modules/extract_data.py
@@ -1,70 +1,87 @@
-import pandas as pd
-from openpyxl import load_workbook  # noqa: F401
-from openpyxl.utils import get_column_letter  # noqa: F401
-import re
-import ast
+"""This module contains various functions related to extracting and/or formatting data from Active Directory, as well as generating the external files."""
 
-cn_pattern = r"CN=([^,]+)"
+# pylint: disable=protected-access
+# Standard library imports
+import ast
+import re
+
+# Public library imports
+from openpyxl.utils import get_column_letter
+import pandas as pd
+
+# Personal library imports
+# Module imports
+# Initialized variables
+CN_PATTERN = r"CN=([^,]+)"
+FLAG_MAP = {
+    "Account_Disabled": 0x2,
+    "Pwd_Not_Required": 0x20,
+    "Pwd_Cant_Change": 0x40,
+    "Pwd_Doesnt_Expire": 0x10000,
+}
 
 
 def extract_ad_data(input_data, the_attributes):
+    """Extracts all requested data from Active Directory."""
     data = []
     for entry in input_data:
         row = {}
         for attr in the_attributes:
             try:
                 if attr == "userAccountControl":
-                    # Column names and flag values
-                    flag_map = {
-                        "Account_Disabled": 0x2,
-                        "Pwd_Not_Required": 0x20,
-                        "Pwd_Cant_Change": 0x40,
-                        "Pwd_Doesnt_Expire": 0x10000,
-                    }
-                    # Set default "N/A" for all flag columns
-                    for column_name in flag_map.keys():
-                        row[column_name] = "N/A"
-                    # Check for set flags
-                    for column_name, flag_value in flag_map.items():
-                        if column_name == "Account_Disabled":
-                            row[column_name] = "Yes" if is_user_enabled(entry) else "No"
-                        elif column_name == "Pwd_Not_Required":
-                            row[column_name] = "Yes" if is_password_not_required(entry) else "No"
-                        elif column_name == "Pwd_Cant_Change":
-                            row[column_name] = "Yes" if is_password_cant_change(entry) else "No"
-                        elif column_name == "Pwd_Doesnt_Expire":
-                            row[column_name] = "Yes" if is_password_doesnt_expire(entry) else "No"
-                elif attr == "pwdLastSet" or attr == "modifyTimeStamp" or attr == "accountExpires" or attr == "createTimeStamp":
-                    if attr == "modifyTimeStamp":
-                        attr_name = "Date_Modified"
-                    else:
-                        attr_name = attr
-                    value = str(entry[attr].values[0])[:10]
-                    if value == "1601-01-01" or value == "9999-12-31":
-                        value = "Never"
-                elif attr == "member":  # Attributes that contain multiple items must be defined here.
-                    attr_name = attr
-                    old_value = entry[attr].values
-                    new_data = []
-                    for item in old_value:
-                        new_value = re.search(cn_pattern, item)
-                        if new_value:
-                            new_data.append(new_value.group(1))
-                    if not new_data:
-                        value = ""
-                    else:
-                        value = new_data
+                    row.update(extract_user_account_control(entry, FLAG_MAP))
+                elif attr in ["pwdLastSet", "modifyTimeStamp", "accountExpires", "createTimeStamp"]:
+                    attr_name = attr if attr != "modifyTimeStamp" else "Date_Modified"
+                    row[attr_name] = extract_timestamp(entry[attr].values[0])
+                elif attr == "member":
+                    row["member"] = extract_member(entry[attr].values)
                 elif attr == "msDS-parentdistname":
-                    attr_name = "Parent_Location"
-                    value = entry[attr].values[0]
+                    row["Parent_Location"] = entry[attr].values[0]
                 else:
-                    attr_name = attr
-                    value = entry[attr].values[0]
-                row[attr_name] = value
-            except (AttributeError, IndexError):  # Handle both missing attributes and empty value lists
-                row[attr_name] = ""
+                    row[attr] = entry[attr].values[0]
+            except (AttributeError, IndexError):
+                row[attr] = ""
         data.append(row)
     return data
+
+
+def extract_user_account_control(entry, flags):
+    """Helper function for user_account_control attribute. Splits the user account control flags into seperate columns and returns a dictionary."""
+    col = {column_name: "N/A" for column_name in flags.keys()}
+    if is_user_enabled(entry):
+        col["Account_Disabled"] = "Yes"
+    else:
+        col["Account_Disabled"] = "No"
+    if is_password_not_required(entry):
+        col["Pwd_Not_Required"] = "Yes"
+    else:
+        col["Pwd_Not_Required"] = "No"
+    if is_password_cant_change(entry):
+        col["Pwd_Cant_Change"] = "Yes"
+    else:
+        col["Pwd_Cant_Change"] = "No"
+    if is_password_doesnt_expire(entry):
+        col["Pwd_Doesnt_Expire"] = "Yes"
+    else:
+        col["Pwd_Doesnt_Expire"] = "No"
+    return col
+
+
+def extract_timestamp(value):
+    """Helper function to convert datetime attributes into a date. Also converts 1601-01-01 and 999-12-31 into "Never" when they show up."""
+    if str(value)[:10] in ["1601-01-01", "9999-12-31"]:
+        return "Never"
+    return str(value)[:10]
+
+
+def extract_member(value):
+    """Helper function to convert member attribute into a list, or blank string if appropiate."""
+    new_data = []
+    for item in value:
+        new_value = re.search(CN_PATTERN, item)
+        if new_value:
+            new_data.append(new_value.group(1))
+    return new_data if new_data else ""
 
 
 def extract_and_format_ad_data(input_data, attributes):
@@ -81,30 +98,36 @@ def search_and_extract_data(base_dn, search_filter, attributes, the_connection):
 
 
 def is_user_enabled(entry):
+    """Checks if the account is enabled."""
     uac = entry["userAccountControl"][0]  # Get the userAccountControl value
     return uac & 0x2  #  Check if the flag is NOT set
 
 
 def is_password_not_required(entry):
+    """Checks if the account requires a password."""
     uac = entry["userAccountControl"][0]
     return uac & 0x20
 
 
 def is_password_cant_change(entry):
+    """Checks if the account cannot change its password."""
     uac = entry["userAccountControl"][0]
     return uac & 0x40
 
 
 def is_password_doesnt_expire(entry):
+    """Checks if the account password does not expire."""
     uac = entry["userAccountControl"][0]
     return uac & 0x10000
 
 
 def create_csv(input_data, filename):
+    """Creates a CSV file."""
     input_data.to_csv(filename, index=False)
 
 
 def create_xlsx(input_data, filename):
+    """Creates an Excel file."""
     excel_writer = pd.ExcelWriter(filename)
     for item in input_data:
         input_data[item] = input_data[item].reset_index(drop=True)
@@ -128,9 +151,7 @@ def create_xlsx(input_data, filename):
                 cell.alignment = new_alignment
 
                 # Newline insertion (conditional)
-                # print(cell.value, type(cell.value))
                 if cell.value.startswith("[") and cell.value.endswith("]"):
-                    print("Cell Value:", cell.value, "Type:", type(cell.value))
                     list_version = ast.literal_eval(cell.value)  # Convert to list
                     cell.value = "\n".join(list_version)  # Convert back to string with newlines
 
@@ -138,6 +159,8 @@ def create_xlsx(input_data, filename):
 
 
 def create_all_reports(extracted_data, config):
+    """Creates all reports, in CSV, XLSX, or both types. XLSX will contain all data, where CSV will have a seperate file per data point.
+    Requires passing of configuration for the export type as a dictionary item "export_type" = "CSV" or "XLSX" or "BOTH"."""
     if config["export_type"] in ["CSV", "BOTH"]:
         for item in extracted_data:
             create_csv(extracted_data[item], f"{item}_report.csv")

6e49b2169157550531a5bfc34b20397dd706fe72
nightpanda2810 <nightpanda2810@gmail.com>
2024-04-30 12:55:32 -0400

    Made minor adjustments to closer conform to Pylint.

--- modules/connect_ad.py
+++ modules/connect_ad.py
@@ -1,3 +1,5 @@
+"""This module is purely to connect to Active Directory."""
+
 # Public library imports
 import ldap3 as ad
 
@@ -12,6 +14,7 @@ import ldap3 as ad
 
 
 def connect_to_ad(server, domain, username, password):
+    """Connects to Active Directory. Requires server name, domain name, username, and password."""
     server = ad.Server(f"{server}.{domain}", get_info=ad.ALL)
     conn = ad.Connection(server, user=(f"{username}@{domain}"), password=password, auto_bind=True)
     return conn

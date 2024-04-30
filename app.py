222a11c65e6a7dc85c8f1b2f55cf6d14a4b9eedd
nightpanda2810 <nightpanda2810@gmail.com>
2024-04-30 12:56:38 -0400

    Made minor adjustments to closer conform to Pylint.

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
+"""This application extracts data from Active Directory and exports to CSV and/or Excel files.
+It uses the Pandalibs library for YAML configuration and printing dictionarys in a readable manner when necessary."""
+
+# Standard library imports
 # Public library imports
-
 # Personal library imports
-from pandalibs.yaml_importer import get_configuration_data
 from pandalibs.pprint_nosort import pp
-
+from pandalibs.yaml_importer import get_configuration_data
 
 # Module imports
-from modules.extract_data import search_and_extract_data, create_all_reports
 from modules.connect_ad import connect_to_ad
+from modules.extract_data import search_and_extract_data, create_all_reports
 
 # Initialized variables
 config = get_configuration_data(up_a_level=False)
-search_filter_users = "(&(objectCategory=person)(objectClass=user)(sAMAccountName=*))"  # Find all users
-search_filter_comps = "(&(objectCategory=computer)(objectClass=user)(sAMAccountName=*))"  # Find all computers
-search_filter_grups = "(&(objectCategory=group)(sAMAccountName=*))"  # Find all groups
+SEARCH_FILTER_USERS = "(&(objectCategory=person)(objectClass=user)(sAMAccountName=*))"
+SEARCH_FILTER_COMPS = "(&(objectCategory=computer)(objectClass=user)(sAMAccountName=*))"
+SEARCH_FILTER_GROUPS = "(&(objectCategory=group)(objectClass=group)(sAMAccountName=*))"
 
-
-# Remainder of the code
 
 # Print out the current configuraion if debugging is set.
 if config["DEBUG"]:
@@ -34,9 +34,9 @@ connection = connect_to_ad(
 
 # Extract data
 extracted_data = {}
-extracted_data["user_data"] = search_and_extract_data(config["base_dn"], search_filter_users, config["required_attributes"], connection)
-extracted_data["comp_data"] = search_and_extract_data(config["base_dn"], search_filter_comps, config["required_attributes"], connection)
-extracted_data["grup_data"] = search_and_extract_data(config["base_dn"], search_filter_grups, config["group_attributes"], connection)
+extracted_data["user_data"] = search_and_extract_data(config["base_dn"], SEARCH_FILTER_USERS, config["user_attributes"], connection)
+extracted_data["comp_data"] = search_and_extract_data(config["base_dn"], SEARCH_FILTER_COMPS, config["comp_attributes"], connection)
+extracted_data["group_data"] = search_and_extract_data(config["base_dn"], SEARCH_FILTER_GROUPS, config["group_attributes"], connection)
 
 # Export data to files.
 create_all_reports(extracted_data, config)
